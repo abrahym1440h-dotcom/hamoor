adc047c50a3bc4469b8d5f3afd4f4fadca1161a5
abrahym1440h-dotcom <abrahym1440h@gmail.com>
2026-05-17 01:05:03 +0300

    Update route.js

--- app/api/sector/route.js
+++ app/api/sector/route.js
@@ -19,7 +19,7 @@ export async function POST(request) {
 }`;
 
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.2-flash:generateContent?key=${apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
